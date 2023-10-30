4c3cdb4c4bee1b273d1e744af0141dbada6e8c41
Yeggnog <jacollins128@gmail.com>
2023-10-30 15:21:44 -0600

    Fixed scaling and double-vision issues with raycast cursor when ported to device

--- Assets/Scripts/RaycastCursor.cs
+++ Assets/Scripts/RaycastCursor.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class RaycastCursor : MonoBehaviour{
     [SerializeField] private SpriteRenderer cursorImg;
     [SerializeField] private Sprite[] images; // move, stairs, item, enemy
+    [SerializeField] private float baseCursorScale = 5f;
 
     Vector3 savedLookDir = Vector3.zero;
+    Vector3 baseCursorOffset = new Vector3(0f, -0.015f, 0f);
+    float minCursorDistance = 0.05f;
 
     void Start(){
         GridMove.GridUpdateEvent += this.updateCursor;
         savedLookDir = transform.forward;
+
+        Vector3 toCursorImg = (cursorImg.gameObject.transform.position - transform.position);
+        toCursorImg = new Vector3(toCursorImg.x, 0f, toCursorImg.z);
+        minCursorDistance = Mathf.Max(toCursorImg.magnitude, Camera.main.nearClipPlane*1.5f);
+        updateCursor();
     }
 
     void Update(){
@@ -25,6 +33,7 @@ public class RaycastCursor : MonoBehaviour{
         // reset cursor image
         cursorImg.sprite = images[0];
 
+        // update image
         RaycastHit hit;
         if(Physics.Raycast(transform.position, savedLookDir, out hit, GridMove.gridSize)){
             if(hit.collider.CompareTag("Enemy")){
@@ -35,5 +44,21 @@ public class RaycastCursor : MonoBehaviour{
                 cursorImg.sprite = images[1];
             }
         }
+
+        // set position/scale of image (to offset double-vision effect)
+        if(Physics.Raycast(transform.position, savedLookDir, out hit)){
+            float castDistance = Mathf.Max(((hit.point - transform.position).magnitude * 0.5f), minCursorDistance);
+            scaleByDistance(castDistance);
+        }else{
+            // reset position / scale
+            scaleByDistance(minCursorDistance);
+        }
+    }
+
+    void scaleByDistance(float distance){
+        // set scale based on FOV and distance from the camera (to keep a uniform view size across distance)
+        float scaleFactor = ((distance * minCursorDistance) * -Mathf.Tan(Camera.main.fieldOfView / 2f) * 1.6f * baseCursorScale);
+        cursorImg.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+        cursorImg.transform.position = ( transform.position + (Camera.main.transform.forward * distance) + (baseCursorOffset * scaleFactor) );
     }
 }
